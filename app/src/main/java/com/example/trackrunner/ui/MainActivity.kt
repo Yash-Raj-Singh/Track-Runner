e8fdceed777ecddf6152248d08c39213421fa51b
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-08-08 11:49:39 +0530

    Added bottom navigation bar, setting up navigation graph to move from one fragment to another.

--- app/src/main/java/com/example/trackrunner/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackrunner/ui/MainActivity.kt
@@ -3,21 +3,33 @@ package com.example.trackrunner.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.trackrunner.R
 import com.example.trackrunner.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()} + Yes, It Is Working...")
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{_, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else ->
+                        bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
