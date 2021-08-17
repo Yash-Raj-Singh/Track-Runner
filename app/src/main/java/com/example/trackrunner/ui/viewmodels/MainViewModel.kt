1ef5866ce721a04a6d685d8a6b6ac17a9de53deb
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-08-17 15:19:32 +0530

    Saving our run's data(screenshot, avgSpeed, Calories Burned, Date, distance, time) in our database.

--- app/src/main/java/com/example/trackrunner/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/trackrunner/ui/viewmodels/MainViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.trackrunner.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.trackrunner.db.Run
 import com.example.trackrunner.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     val mainRepository : MainRepository
 ) : ViewModel() {
+
+    fun insertRun(run : Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
+
 }
