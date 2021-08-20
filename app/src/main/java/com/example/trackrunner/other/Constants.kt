7cff73c9eca16bdc3235f7fc3b62ea2ab9efdb37
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-08-20 20:24:39 +0530

    Saving our name and weight in shared preferences.

--- app/src/main/java/com/example/trackrunner/other/Constants.kt
+++ app/src/main/java/com/example/trackrunner/other/Constants.kt
@@ -14,6 +14,11 @@ object Constants {
 
     const val TIMER_UPDATE_INTERVAL = 50L
 
+    const val SHARED_PREFERENCES_NAME = "sharedPref"
+    const val KEY_FIRST_TIME_TOGGLE = "KEY_FIRST_TIME_TOGGLE"
+    const val KEY_NAME = "KEY_NAME"
+    const val KEY_WEIGHT = "KEY_WEIGHT"
+
     const val POLYLINE_COLOR = Color.GREEN
     const val POLYLINE_WIDTH = 10f
     const val MAP_ZOOM = 18f
